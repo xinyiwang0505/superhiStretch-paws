beb9779f7a44691fc9e36063ea2dddf5c2406b53
Xinyi Wang <xinyiwang.art@gmail.com>
2022-06-02 15:23:59 -0400

    Initial Commit

---
+++ superhiStretch+paws/superhiStretch_pawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  superhiStretch_pawsApp.swift
+//  superhiStretch+paws
+//
+//  Created by Cindy Wang on 6/2/22.
+//
+
+import SwiftUI
+
+@main
+struct superhiStretch_pawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
