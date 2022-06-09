09029c4e2f5b7f034df21b716bf8df44b539e4bf
Xinyi Wang <xinyiwang.art@gmail.com>
2022-06-08 23:36:26 -0400

    content view styled

--- superhiStretch+paws/ContentView.swift
+++ superhiStretch+paws/ContentView.swift
@@ -9,15 +9,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    init(){
+        UITableView.appearance().backgroundColor = UIColor(named:"secondary")
+    }
+    
+    let poses = Poses()
     var body: some View {
         NavigationView{
-            List{
+            List(poses.poseData){pose in
                 NavigationLink(destination: DetailView()) {
-                    Text("Downward-facing Dog")
+                    Image(pose.icon)
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                    Text(pose.name)
+                        .fontWeight(.medium)
+                        .font(.title3)
+                        .padding(.leading,20)
                 }
-                Text("Downward-facing Dog")
-                Text("Downward-facing Dog")
-                Text("Downward-facing Dog")
+                .padding(5)
+                .listRowBackground(Color("secondary"))
+                
             }.listStyle(.grouped)
             
                 .navigationBarTitle("Strech + Paw")
