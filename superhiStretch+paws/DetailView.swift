5cf0d9e7353ff1783584602e080771ff30a8c38c
Xinyi Wang <xinyiwang.art@gmail.com>
2022-06-12 01:34:55 -0400

    Timer View Styled

--- superhiStretch+paws/DetailView.swift
+++ superhiStretch+paws/DetailView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct DetailView: View {
     let pose: Pose
+    @State private var timerOpen = false
     var body: some View {
         ZStack {
             Color("secondary").ignoresSafeArea()
@@ -45,11 +46,14 @@ struct DetailView: View {
                         Text(pose.topTip)
                     }
                    
-                }
-            }.padding(.horizontal,20)
+                }.padding(.horizontal,20)
+            }
                
-            
+            TimerPanelView(timerOpen: $timerOpen)
+        }.onTapGesture {
+            timerOpen = false
         }
+        
     }
 }
 
@@ -63,5 +67,74 @@ struct DetailView_Previews: PreviewProvider {
             "Did someone say dog? Can't we call this a downward-facing cat instead? It's OK – this is a friendly dog, it's not interested in chasing cats. In fact, Downward-Facing Dog is the lynchpin of a yoga asana practice: if you're going to befriend with any of these poses, make sure it's this canine classic.",
             steps: ["From a kneeling position, place your hands shoulder-distance apart and spread your fingers.", "Tuck your toes and lift your hips up towards the ceiling so you create an inverted V shape.", "Balance the weight between hands and feet and think about tilting your tailbone up towards the ceiling.","Send your gaze towards your feet and breath!"],
             topTip: "Bend your knees in order to create more length through the spine." ))
+    }
+}
+
+
+
+struct TimerPanelView: View{
+    @Binding var timerOpen: Bool
+    var body:some View{
+        //if timer panel is closed, show timer closed view
+        //if timer panel is open, show timer open view
+        VStack{
+            Spacer()
+            VStack {
+                timerOpen ? AnyView( TimerOpenView()) : AnyView (TimerClosedView())
+                    
+            }
+            .foregroundColor(Color("secondary"))
+//            .padding(40)
+            .frame(maxWidth:.infinity,maxHeight: timerOpen ? 400 : 80)
+            .background(Color("highlight"))
+            .cornerRadius(6)
+        }
+        .ignoresSafeArea()
+        .onTapGesture {
+            timerOpen.toggle()
+        }
+        
+    }
+    
+}
+
+
+
+struct TimerOpenView: View{
+    var body:some View{
+        VStack{
+            Text("Hold that pose")
+                .fontWeight(.medium)
+            Spacer()
+            Text("Try staying in this pose for 30 seconds. If you need to come out sooner, that's ok.")
+                .multilineTextAlignment(.center)
+           
+            Spacer()
+            Text("00:30")
+                .font(.system(size:96))
+            Spacer()
+            Button{
+                //do something
+            }label:{
+                Text("start the timer")
+            }
+            .frame(maxWidth:300,maxHeight: 50)
+            .background(Color("secondary"))
+            .foregroundColor(Color("primary"))
+            .cornerRadius(30)
+
+//
+        }.padding(30)
+        
+    }
+    
+}
+
+struct TimerClosedView: View {
+    var body: some View {
+        Text("Try it out")
+            .fontWeight(.medium)
+            .padding(20)
+        Spacer()//bump the text up
     }
 }
